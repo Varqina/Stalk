e59080e88b5e46a75436c6f990381682ec8d1f31
Varqina <flisu2@gmail.com>
2019-03-18 21:57:39 +0100

    Created Mapper

--- src/main/java/com/example/Stalk/models/mapper/MessageFormToEntityMapper.java
+++ src/main/java/com/example/Stalk/models/mapper/MessageFormToEntityMapper.java
@@ -1,4 +1,19 @@
 package com.example.Stalk.models.mapper;
 
+import com.example.Stalk.models.entity.MessageEntity;
+import com.example.Stalk.models.forms.AddMessageForm;
+import com.example.Stalk.models.service.HashService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class MessageFormToEntityMapper {
+
+    @Autowired
+    HashService hashService;
+
+    public MessageEntity map(AddMessageForm form) {
+        MessageEntity messageEntity = new MessageEntity();
+        messageEntity.setMessage(form.getMessage());
+        messageEntity.setKey(hashService.hashRandom());
+        return messageEntity;
+    }
 }
