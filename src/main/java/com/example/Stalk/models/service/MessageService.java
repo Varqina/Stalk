f5a88bdbab671c46284491a46f3b3b8ad9ebc6f4
Varqina <flisu2@gmail.com>
2019-03-18 22:06:35 +0100

    MessageService implemented methode saveMessage

--- src/main/java/com/example/Stalk/models/service/MessageService.java
+++ src/main/java/com/example/Stalk/models/service/MessageService.java
@@ -1,4 +1,22 @@
 package com.example.Stalk.models.service;
 
+import com.example.Stalk.models.entity.MessageEntity;
+import com.example.Stalk.models.forms.AddMessageForm;
+import com.example.Stalk.models.mapper.MessageFormToEntityMapper;
+import com.example.Stalk.models.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MessageService {
+
+    @Autowired
+    MessageRepository messageRepository;
+
+
+
+    public void saveMessage(AddMessageForm messageForm){
+        MessageEntity entity = new MessageFormToEntityMapper().map(messageForm);
+        messageRepository.save(entity);
+    }
 }
