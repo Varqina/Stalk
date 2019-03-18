635762d54631e6ed833f8296fe7bf4ac06240551
Varqina <flisu2@gmail.com>
2019-03-18 21:57:26 +0100

    HashService added

---
+++ src/main/java/com/example/Stalk/models/service/HashService.java
@@ -0,0 +1,19 @@
+package com.example.Stalk.models.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class HashService {
+
+    public String hashRandom(){
+        StringBuilder hash = new StringBuilder();
+        Random random = new Random();
+        for ( int i = 0; i < 10;i++){
+            hash.append(random.nextInt(9));
+        }
+
+        return hash.toString();
+    }
+}
