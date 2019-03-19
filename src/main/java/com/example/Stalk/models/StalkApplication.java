fc074c0bbf1dc555029ba8f426184b0a3241146e
Varqina <flisu2@gmail.com>
2019-03-19 21:49:46 +0100

    FoldersOrderFixed

--- src/main/java/com/example/Stalk/models/StalkApplication.java
+++ src/main/java/com/example/Stalk/models/StalkApplication.java
@@ -1,4 +1,4 @@
-package com.example.Stalk;
+package com.example.Stalk.models;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
