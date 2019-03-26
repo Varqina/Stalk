31c013065cdb68537c04c85198547baef6c29d6b
Varqina <flisu2@gmail.com>
2019-03-26 22:43:42 +0100

    Extended Method for get Count of ID and byId

--- src/main/java/com/example/Stalk/models/repository/MessageRepository.java
+++ src/main/java/com/example/Stalk/models/repository/MessageRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,9 @@ public interface MessageRepository extends CrudRepository <MessageEntity,Integer
     @Query(value="SELECT * FROM `message` WHERE `public_key` = ?1",nativeQuery = true)
     Optional<MessageEntity> getMessageByPublicKey(String publicKey);
 
+    @Query(value="SELECT ID FROM MESSAGE ",nativeQuery = true)
+    List getIdFromDataBase();
+
+    @Query(value="SELECT * FROM `message` WHERE `id` = ?1",nativeQuery = true)
+    Optional<MessageEntity> getMessageById(Integer id);
 }
